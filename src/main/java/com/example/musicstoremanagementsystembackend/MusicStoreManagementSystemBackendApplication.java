2379d8da81c5b7ec4c6c137124727f8b18a66011
jonx1183 <jonx1183@stud.kea.dk>
2023-08-23 15:12:33 +0200

    Initial commit

---
+++ src/main/java/com/example/musicstoremanagementsystembackend/MusicStoreManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicstoremanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreManagementSystemBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MusicStoreManagementSystemBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/musicstoremanagementsystembackend/MusicStoreManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicstoremanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreManagementSystemBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
